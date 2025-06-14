3d53ddc2a1b3ee59e73ff1b66f3069690a76fa1a
Glenn Haertlein <htack210@live.com>
2025-06-14 16:33:16 -0400

    Changes

--- PrimAndFloatsMaven/prim_and_floats/src/main/java/com/haertlein/Main.java
+++ PrimAndFloatsMaven/prim_and_floats/src/main/java/com/haertlein/Main.java
@@ -1,7 +1,24 @@
 package com.haertlein;
+/* Glenn Haertlein  */
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        float max = Float.MAX_VALUE;
+        float min = Float.MIN_VALUE;
+        double dblMax = Double.MAX_VALUE;
+        double dblMin = Double.MIN_VALUE;
+
+        System.out.printf("""
+                          Float Max = %.2e
+                          """, max);
+        System.out.printf("""
+                          Float Min = %.2e
+                          """, -min);
+        System.out.printf("""
+                          Double Max = %.2e
+                          """, dblMax);
+        System.out.printf("""
+                          Double Min = %.2e
+                          """, -dblMin);
     }
 }
