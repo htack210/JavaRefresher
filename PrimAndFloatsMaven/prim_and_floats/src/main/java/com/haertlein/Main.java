7b7b3415d9869a16bebb41e398494ac3f97a99e4
Glenn Haertlein <htack210@live.com>
2025-06-14 18:38:19 -0400

    stuff

--- PrimAndFloatsMaven/prim_and_floats/src/main/java/com/haertlein/Main.java
+++ PrimAndFloatsMaven/prim_and_floats/src/main/java/com/haertlein/Main.java
@@ -2,11 +2,17 @@ package com.haertlein;
 /* Glenn Haertlein  */
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         float max = Float.MAX_VALUE;
         float min = Float.MIN_VALUE;
         double dblMax = Double.MAX_VALUE;
         double dblMin = Double.MIN_VALUE;
+
+        System.setOut(new java.io.PrintStream(System.out, true, "UTF-8"));
+        System.out.println('\u00A7');
+        char var2 = '\u00A7';
+
+        System.out.println(var2);
 
         System.out.printf("""
                           Float Max = %.2e
