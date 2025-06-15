41e98b475a31f4b6b1da5fb805650abbb2c95663
Glenn Haertlein <htack210@live.com>
2025-06-14 21:27:30 -0400

    start type casting basics project

--- StringBasics/stringbasics/src/main/java/com/haertlein/Main.java
+++ StringBasics/stringbasics/src/main/java/com/haertlein/Main.java
@@ -2,6 +2,12 @@ package com.haertlein;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char symb = '\u00A9';
+        String var = "Glenn";
+        String var2 = "Haertlein";
+        int num = 14;
+
+        System.out.println(var + " " + var2 + " " + symb + " " + num);
+        System.out.println("This is a Unicode test: \u03A9, \u03C0, \u4E2D, \uD83D\uDE80");
     }
 }
