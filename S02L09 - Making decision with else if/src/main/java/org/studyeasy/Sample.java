0619f25112b7c5359645c1f9b5fc5e6506114286
Glenn Haertlein <htack210@live.com>
2025-06-19 15:43:06 -0400

    Format code

--- S02L09 - Making decision with else if/src/main/java/org/studyeasy/Sample.java
+++ S02L09 - Making decision with else if/src/main/java/org/studyeasy/Sample.java
@@ -7,18 +7,16 @@ public class Sample {
         int x = 4;
         int y = 10;
 
-        if (x == 5){
+        if (x == 5) {
             System.out.println("X is 5");
-        }else if(x > 5){
+        } else if (x > 5) {
             System.out.println("X is greater 5");
-        }else{
+        } else {
             System.out.println("X is smaller 5");
-            if (y == 10){
+            if (y == 10) {
                 System.out.println("y is 10");
             }
         }
-
-
 
     }
 }
