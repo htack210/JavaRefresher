9eb8d10d55880468c901eb2e211321634178c3a3
Glenn Haertlein <htack210@live.com>
2025-06-19 12:51:39 -0400

    Format the code

--- S02L03 - Making decision with operators - (Part 01)/src/main/java/org/studyeasy/Sample.java
+++ S02L03 - Making decision with operators - (Part 01)/src/main/java/org/studyeasy/Sample.java
@@ -5,9 +5,9 @@ public class Sample {
     public static void main(String[] args) {
         int x = 6;
 
-        if ( x != 6) {
+        if (x != 6) {
             System.out.println("X is not 6");
-        }else {
+        } else {
             System.out.println("X is 6");
         }
 
