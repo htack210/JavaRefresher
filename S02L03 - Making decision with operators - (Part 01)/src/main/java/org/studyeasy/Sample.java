2e4d4097f6dcf545ff584b927d30944ec997c57e
Glenn Haertlein <htack210@live.com>
2025-06-19 16:10:27 -0400

    Use modern switch syntax

--- S02L03 - Making decision with operators - (Part 01)/src/main/java/org/studyeasy/Sample.java
+++ S02L03 - Making decision with operators - (Part 01)/src/main/java/org/studyeasy/Sample.java
@@ -3,15 +3,22 @@ package org.studyeasy;
 
 public class Sample {
     public static void main(String[] args) {
-        int x = 6;
+        int x = 3;
 
-        if (x != 6) {
-            System.out.println("X is not 6");
-        } else {
-            System.out.println("X is 6");
+        // if (x != 6) {
+        // System.out.println("X is not 6");
+        // } else {
+        // System.out.println("X is 6");
+        // }
+
+        // System.out.println("Sample code");
+
+        switch (x) {
+            case 1 -> System.out.println("x is 1");
+            case 2 -> System.out.println("x is 2");
+            case 3 -> System.out.println("x is 3");
+            default -> System.out.println("x is " + x);
         }
-
-        System.out.println("Sample code");
 
     }
 }
