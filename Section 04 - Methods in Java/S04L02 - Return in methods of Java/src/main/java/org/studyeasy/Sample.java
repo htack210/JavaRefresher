53c972c10045c50d749564a73eacebe669cbfe10
Glenn Haertlein <htack210@live.com>
2025-06-20 14:02:46 -0400

    use return value directly.

--- Section 04 - Methods in Java/S04L02 - Return in methods of Java/src/main/java/org/studyeasy/Sample.java
+++ Section 04 - Methods in Java/S04L02 - Return in methods of Java/src/main/java/org/studyeasy/Sample.java
@@ -3,8 +3,8 @@ package org.studyeasy;
 public class Sample {
     public static void main(String[] args) {
 
-        int area = areaOfRectangle(10,15);
-        System.out.println("Area of the shape :"+ area);
+        // int area = areaOfRectangle(10,15);
+        System.out.println("Area of the shape :"+ areaOfRectangle(10,15));
 
 
     }
